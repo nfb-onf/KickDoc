5d2df39eb1e7f7bb615f32af6ba3b8aa53c71d5c
gporras <g.porras@onf.ca>
2015-04-24 15:12:28 -0400

    Added hackaton documentary

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,3 +28,21 @@ suckr = ImageSuckr::GoogleSuckr.new
     :status => [:new, :fund, :completed].sample
   )
 end
+
+Documentary.create(
+    :title => "Hackaton the documenrary",
+    :genres => [@genres[rand(@genres.count)]],
+    :short_synopsis => "This is a great documentary about the hackaton canayan culture",
+    :long_synopsis => "This is a great great Awsome documentary about the hackaton canayan culture",
+    :backers => rand(200),
+    :expired_at => DateTime.now + 30,
+    :creator => Faker::Name.name,
+    :percent_funded => rand(100),
+    :city => Faker::Address.city,
+    :budget => rand(10000..500000),
+    :purpose => "To give voice to the hackers",
+    :thumbnail_url => "http://blog.monitis.com/wp-content/uploads/2014/09/Hackathon-2.jpg",
+    :pitch_video_url => "https://player.vimeo.com/video/125937809",
+    :approved => true,
+    :status => :completed
+)
