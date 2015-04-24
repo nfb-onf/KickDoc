35319a3e763b1d283dbed4f281a1dbe612a9b938
Jerome Lefeuvre <j.lefeuvre@onf.ca>
2015-04-24 10:01:49 -0400

    Renaming genres

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-%w(animation classique fiction grands_enjeux femmes_au_cinema peuples_autochtones).each do |genre|
+%w(animated_film children_film documentary experimental_film fiction interview).each do |genre|
   Genre.create(:title => genre)
 end
 
